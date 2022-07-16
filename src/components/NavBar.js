1791d1450271efcfa5ce89d977f2eddf06f453ad
caudencia wanyama <conny@students.uonbi.ac.ke>
2022-07-17 02:39:53 +0300

    Done.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,13 @@ import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
+  const myLink=links.map((link)=>
+  <a href={ '#' +  link} key={link}>{link}</a>
+  )
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return <nav>
+    {myLink}
+  </nav>;
 }
 
 export default NavBar;
